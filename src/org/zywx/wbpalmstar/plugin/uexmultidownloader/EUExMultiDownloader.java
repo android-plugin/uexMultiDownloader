8cd49e9c8cf45e7fe1795d8e23320551966ce631
yanlongtao <longtao.yan@3g2win.com>
2015-07-07 16:09:24 +0800

    init

---
+++ src/org/zywx/wbpalmstar/plugin/uexmultidownloader/EUExMultiDownloader.java
@@ -0,0 +1,47 @@
+package org.zywx.wbpalmstar.plugin.uexmultidownloader;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.text.TextUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.zywx.wbpalmstar.engine.EBrowserView;
+import org.zywx.wbpalmstar.engine.universalex.EUExBase;
+
+public class EUExMultiDownloader extends EUExBase {
+
+    private static final String BUNDLE_DATA = "data";
+
+    public EUExMultiDownloader(Context context, EBrowserView eBrowserView) {
+        super(context, eBrowserView);
+    }
+
+    @Override
+    protected boolean clean() {
+        return false;
+    }
+    
+
+    @Override
+    public void onHandleMessage(Message message) {
+        if(message == null){
+            return;
+        }
+        Bundle bundle=message.getData();
+        switch (message.what) {
+
+        default:
+                super.onHandleMessage(message);
+        }
+    }
+
+    private void callBackPluginJs(String methodName, String jsonData){
+        String js = SCRIPT_HEADER + "if(" + methodName + "){"
+                + methodName + "('" + jsonData + "');}";
+        onCallback(js);
+    }
+
+}
---
+++ src/org/zywx/wbpalmstar/plugin/uexmultidownloader/JsConst.java
@@ -0,0 +1,4 @@
+package org.zywx.wbpalmstar.plugin.uexmultidownloader;
+
+public class JsConst {
+}
